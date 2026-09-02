e67e9c72cceea4a940558eba1a5d0def93cc7ecc
Nina Zhang <ninazhang@Ninas-MacBook-Air-2.local>
2026-09-02 16:48:35 -0400

    commenting for clearer understanding

--- tb_analysis.py
+++ tb_analysis.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# PROMPT 1: Reading CSV into script, displaying summary of first 5 
+# --- PROMPT 1: Reading CSV into script, displaying summary of first 5 
 
 # Read the file 
 csv_filename = "Code_Assessment_Data.csv"
 df = pd.read_csv(csv_filename)
 
+# printed in terminal
 print("First 5 Rows")
 print(df.head())
 
@@ -14,12 +15,11 @@ num_rows, num_cols = df.shape
 print(f"\nNumber of Rows: {num_rows}")
 print(f"\nNumber of Columns: {num_cols}")
 
-# PROMPT 2: Create plot showing distribution of "Ave Life-months" in years
-
-df['Ave Life-years'] = df['Ave Life-months'] / 12
-
-# using histogram to analyze distribution 
+# --- PROMPT 2: Create plot showing distribution of "Ave Life-months" in years
+# set to figure size 8,5 for viewing so graph isn't squished
 plt.figure(figsize=(8,5))
+df['Ave Life-years'] = df['Ave Life-months'] / 12
+# using histogram to analyze distribution 
 plt.hist(df['Ave Life-years'], bins=10)
 plt.title('Distribution of Average Life-Years Across Simulation Runs', fontweight='bold')
 plt.xlabel('Average Life-Years')
@@ -31,24 +31,22 @@ plt.tight_layout()
 
 # saving file as png. dpi = 300 for standard high res quailty
 plt.savefig('distribution_life_years.png', dpi = 300)
-plt.show()
 
-# PROMPT 3: Average Life-months vs. Lifetime costs 
+# --- PROMPT 3: Average Life-months vs. Lifetime costs 
 
 # using scatterplot to analyze the relationship
 plt.figure(figsize=(8,5))
 plt.scatter(df['Ave Life-months'], df['Lifetime Costs'])
 plt.title('Lifetime Costs vs. Average Life-Months', fontweight='bold')
 plt.xlabel('Average Life-Months')
-plt.ylabel('Lifetime Costs (dollars)')
+plt.ylabel('Lifetime Costs (USD)')
 # formatting for easier view 
 plt.grid(linestyle = "--")
 plt.tight_layout()
 
 plt.savefig('life_months_vs_costs.png', dpi=300)
-plt.show() 
 
-# PROMPT 4: Calculate the ratio of new TB infection to TB activations, save to new CSV 
+# --- PROMPT 4: Calculate the ratio of new TB infection to TB activations, save to new CSV 
 ratios_df = pd.DataFrame({'Run ID': df['Run ID']})
 
 # 3 years of data: 36 months 
